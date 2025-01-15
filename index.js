3bcf492c6838547a7cae45366d15afed372a41bc
userabiii <abirami060503@gmail.com>
2025-01-15 16:43:22 +0530

    Abi file

---
+++ index.js
@@ -0,0 +1,9 @@
+function subscribe(){
+    const buttonElement = document.querySelector('button');
+    if(buttonElement.innerHTML=== 'Subscribe'){
+        buttonElement.innerHTML = 'Subscribed'
+    }
+    else{
+        buttonElement.innerHTML = 'Subscribe'
+    }   
+}
